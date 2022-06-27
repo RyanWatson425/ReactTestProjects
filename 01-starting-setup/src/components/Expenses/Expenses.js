58ddb637f62cba290778970dbcc858c6c6dae83e
RyanWatson425 <watsonr3@spu.edu>
2022-06-27 14:29:23 -0700

    Dynamic List Rendering
    The page now dynamically renders the list of expenses. It will now show expenses that are added to the list.

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filterVal, setFilterVal] = useState('2020');
+  const [filterVal, setFilterVal] = useState("2020");
 
   const saveFilterValueHandler = (filterValue) => {
     setFilterVal(filterValue);
@@ -13,28 +13,19 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className="expenses">
-        <ExpensesFilter selected={filterVal} onSaveFilterValue={saveFilterValueHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filterVal}
+          onSaveFilterValue={saveFilterValueHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
