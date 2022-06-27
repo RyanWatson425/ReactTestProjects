8d1cb141c8e0b980f4f021da903717135aa3a601
RyanWatson425 <watsonr3@spu.edu>
2022-06-27 16:31:41 -0700

    refactoring

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date = {props.date}/>
-      <div className="expense-itemdescription">
-        <h2>{props.title}</h2>
-        <div className="expense-itemprice">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-itemdescription">
+          <h2>{props.title}</h2>
+          <div className="expense-itemprice">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filterVal, setFilterVal] = useState('2020');
+  const [filterVal, setFilterVal] = useState("2020");
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filterVal;
-  })
+  });
 
   const saveFilterValueHandler = (filterValue) => {
     setFilterVal(filterValue);
@@ -17,19 +17,12 @@ const Expenses = (props) => {
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filterVal}
           onSaveFilterValue={saveFilterValueHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
