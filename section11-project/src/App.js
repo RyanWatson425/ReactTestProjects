f9d22fef48b13d4b91c6d7f9efd1d811721cde50
RyanWatson425 <watsonr3@spu.edu>
2022-07-24 21:41:27 -0700

    Added modal for the cartButton in the header
    now opens when clicked and closes when clicking the backdrop or the order/close buttons on the modal

--- section11-project/src/App.js
+++ section11-project/src/App.js
@@ -1,16 +1,26 @@
-import Card from './components/UI/Card';
 import Header from './components/Layout/Header';
-import React from 'react';
+import React, { useState } from 'react';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-      {/* <Card/> */}
     </React.Fragment>
   );
 }
