22db0cf635247bf35399089ae6bda804c087e433
RyanWatson425 <watsonr3@spu.edu>
2022-07-30 14:11:49 -0700

    Added ErrorBoundary example

--- section13-project/src/components/Users.js
+++ section13-project/src/components/Users.js
@@ -17,6 +17,12 @@ class Users extends Component {
     });
   }
 
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error('No Users Provided!');
+    }
+  }
+
   render () {
     const usersList = (
       <ul>
