4279d2f77f2f8787db82c1179f7f6885828ca0d7
RyanWatson425 <watsonr3@spu.edu>
2022-07-30 13:53:13 -0700

    Using Context in class components

--- section13-project/src/components/UserFinder.js
+++ section13-project/src/components/UserFinder.js
@@ -1,47 +1,44 @@
-import { Fragment, Component, useState, useEffect } from "react";
-import classes from "./UserFinder.module.css";
+import { Fragment, useState, useEffect, Component } from 'react';
 
-import Users from "./Users";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import Users from './Users';
+import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
-      searchTerm: "",
+      filteredUsers: [],
+      searchTerm: '',
     };
   }
 
-  searchChangeHandler(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
-
   componentDidMount() {
-    //send http request...
-    //this.setState({ filteredUsers: DUMMY_USERS });
+    // Send http request...
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
     }
   }
 
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <Fragment>
         <div className={classes.finder}>
-          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
         </div>
         <Users users={this.state.filteredUsers} />
       </Fragment>
